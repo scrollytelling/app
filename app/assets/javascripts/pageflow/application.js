95a683ecbb8384c9f79bc6e5c39a495a03871e97
Joost Baaij <joost@spacebabies.nl>
2015-12-08 21:38:05 +0100

    upgrade pageflow-before-after

--- app/assets/javascripts/pageflow/application.js
+++ app/assets/javascripts/pageflow/application.js
@@ -1,5 +1,5 @@
 //= require pageflow/base
 //= require pageflow/before_after
 //= require pageflow/text_page
-//= require "pageflow/embedded_video"
-//= require "pageflow/internal_links"
+//= require pageflow/embedded_video
+//= require pageflow/internal_links
