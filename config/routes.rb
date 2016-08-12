58a3890a4f7b500a6b44ff87bdce7e3617877218
Joost Baaij <joost@spacebabies.nl>
2016-08-12 14:50:45 +0200

    prevent requests to /piwik.js from triggering Sentry

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # route /piwik.js to a 404 since the Facebook spider keeps requesting this.
+  get "/piwik.js", to: proc {|env| [404, {"Content-Type" => "text/plain"}, ["You're drunk."]] }
+
   constraints HostConstraint.new do
     devise_for :users, ActiveAdmin::Devise.config
     ActiveAdmin.routes(self)
