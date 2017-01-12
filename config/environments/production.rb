e60e0ae2c333b6230d5579fe5a4e1615f5e86469
Joost Baaij <joost@spacebabies.nl>
2017-01-12 16:17:28 +0100

    switch to starttls auto for sendgrid
    we're moving away from mailgun!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     user_name:            ENV['SMTP_USER'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
-    enable_starttls_auto: false
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
