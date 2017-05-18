9756de2919381c9ca1a9f61ed13bb02fc123ced3
Joost Baaij <joost@spacebabies.nl>
2017-05-18 09:17:10 +0200

    don't use ruby 2.4 code on 2.3 ....

--- config/initializers/host_constraint.rb
+++ config/initializers/host_constraint.rb
@@ -6,6 +6,9 @@
 class HostConstraint
   def matches?(request)
     return true if Rails.env.in? %w(test development)
-    /scrollytelling.io\z/.match?(request.host)
+    request.host =~ /scrollytelling.io\z/
+
+    # from Ruby 2.4 onward use this:
+    # /scrollytelling.io\z/.match?(request.host)
   end
 end
