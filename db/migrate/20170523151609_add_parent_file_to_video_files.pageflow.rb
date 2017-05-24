f7ad6ff04afab26aa77ee3f8b38b036aabbf1d47
Joost Baaij <joost@spacebabies.nl>
2017-05-24 09:08:43 +0200

    limit indexed string columns to varchar(191)
    http://blog.arkency.com/2015/05/how-to-store-emoji-in-a-rails-app-with-a-mysql-database/

--- db/migrate/20170523151609_add_parent_file_to_video_files.pageflow.rb
+++ db/migrate/20170523151609_add_parent_file_to_video_files.pageflow.rb
@@ -2,7 +2,7 @@
 class AddParentFileToVideoFiles < ActiveRecord::Migration
   def change
     add_column :pageflow_video_files, :parent_file_id, :integer
-    add_column :pageflow_video_files, :parent_file_model_type, :string
+    add_column :pageflow_video_files, :parent_file_model_type, :string, limit: 191
     add_index :pageflow_video_files, [:parent_file_id, :parent_file_model_type],
               name: 'index_video_files_on_parent_id_and_parent_model_type'
   end
--- db/migrate/20170523151610_add_parent_file_to_audio_files.pageflow.rb
+++ db/migrate/20170523151610_add_parent_file_to_audio_files.pageflow.rb
@@ -2,7 +2,7 @@
 class AddParentFileToAudioFiles < ActiveRecord::Migration
   def change
     add_column :pageflow_audio_files, :parent_file_id, :integer
-    add_column :pageflow_audio_files, :parent_file_model_type, :string
+    add_column :pageflow_audio_files, :parent_file_model_type, :string, limit: 191
     add_index :pageflow_audio_files, [:parent_file_id, :parent_file_model_type],
               name: 'index_audio_files_on_parent_id_and_parent_model_type'
   end
