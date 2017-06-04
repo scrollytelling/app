21fba90d3648c9dc60d2e5d0dde3175564a17393
Joost Baaij <joost@spacebabies.nl>
2017-06-04 21:35:23 +0200

    add staging interpolation via this patch

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,10 @@
+# TODO temporary patch for the absence of :pageflow_filesystem_root
+# We should fix this in Pageflow proper and then kill this file.
+if Rails.env.staging?
+  require 'paperclip'
+  require 'pageflow'
+
+  Paperclip.interpolates(:pageflow_filesystem_root) do |attachment, style|
+    Pageflow.config.paperclip_filesystem_root
+  end
+end
