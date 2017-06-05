a6f771114718cbe0cca2fd1e10fd0fa35a313d29
Joost Baaij <joost@spacebabies.nl>
2017-06-05 17:52:24 +0200

    precompile assets related to react

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '34'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w(react-server.js components.js)
