650e6ab167b16648476ec360a32925ad50c844bf
Joost Baaij <joost@spacebabies.nl>
2017-06-10 12:17:41 +0200

    Revert "force recompile again"
    This reverts commit 1e14b05099d3b0f76ff8ba3206fc0609a8ebc3c3.

--- app/assets/javascripts/pageflow/editor.js
+++ app/assets/javascripts/pageflow/editor.js
@@ -1,6 +1,7 @@
 //= require i18n
 //= require i18n/translations
 
+
 //= require pageflow/editor/base
 //= require pageflow/before_after/editor
 //= require pageflow/text_page/editor
@@ -8,7 +9,3 @@
 //= require pageflow/internal_links/editor
 //= require pageflow/external_links/editor
 //= require pageflow/linkmap_page/editor
-
-function test() {
-  // just a recompile.
-}
