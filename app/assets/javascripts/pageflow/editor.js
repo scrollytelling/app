1e14b05099d3b0f76ff8ba3206fc0609a8ebc3c3
Joost Baaij <joost@spacebabies.nl>
2017-06-10 12:17:19 +0200

    force recompile again

--- app/assets/javascripts/pageflow/editor.js
+++ app/assets/javascripts/pageflow/editor.js
@@ -1,7 +1,6 @@
 //= require i18n
 //= require i18n/translations
 
-
 //= require pageflow/editor/base
 //= require pageflow/before_after/editor
 //= require pageflow/text_page/editor
@@ -9,3 +8,7 @@
 //= require pageflow/internal_links/editor
 //= require pageflow/external_links/editor
 //= require pageflow/linkmap_page/editor
+
+function test() {
+  // just a recompile.
+}
