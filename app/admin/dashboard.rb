8b3192f7929f60288056d5ffafbb82026395ee17
Joost Baaij <joost@spacebabies.nl>
2017-10-14 13:13:01 +0200

    make two nice panels with statuses

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,9 +3,24 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    panel "Wat zeggen de robot overlords over onze Scrollytelling computercode?" do
-      img src: "https://app.codeship.com/projects/7f550840-f8e9-0132-4345-3a92bb520805/status?branch=master"
-      img src: "https://app.cloud66.com/stacks/badge/5ab71f9be03652639847e5da1362cafe.svg", height: 18
+    columns do
+      column do
+        panel "Codeship" do
+          div do
+            img src: "https://app.codeship.com/projects/7f550840-f8e9-0132-4345-3a92bb520805/status?branch=master"
+          end
+          i "Codeship test nieuwe code die wij toevoegen."
+        end
+      end
+
+      column do
+        panel "Cloud 66" do
+          div do
+            img src: "https://app.cloud66.com/stacks/badge/5ab71f9be03652639847e5da1362cafe.svg", height: 18
+          end
+          i "Cloud 66 zet het vervolgens geautomatiseerd online."
+        end
+      end
     end
   end
 end
