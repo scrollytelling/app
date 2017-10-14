6c287c9e70cbae54c0a4aa6cf747545b026be0af
Joost Baaij <joost@spacebabies.nl>
2017-10-14 11:23:01 +0200

    update all gems to latest (where possible)

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,8 +3,9 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    panel "Wat zegt de testrobot over onze artisanale huisgedraaide computercode?" do
-      image_tag "https://app.codeship.com/projects/7f550840-f8e9-0132-4345-3a92bb520805/status?branch=master"
+    panel "Wat zeggen de robot overlords over onze Scrollytelling computercode?" do
+      img src: "https://app.codeship.com/projects/7f550840-f8e9-0132-4345-3a92bb520805/status?branch=master"
+      img src: "https://app.cloud66.com/stacks/badge/5ab71f9be03652639847e5da1362cafe.svg", height: 18
     end
   end
 end
