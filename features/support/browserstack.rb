3c9f2c606b0f2daa2300683bb68a651905bc943f
Joost Baaij <joost@spacebabies.nl>
2017-10-30 15:42:30 +0100

    use Travis -> Browserstack

---
+++ features/support/browserstack.rb
@@ -0,0 +1,26 @@
+require 'selenium-webdriver'
+
+# Input capabilities
+caps = Selenium::WebDriver::Remote::Capabilities.new
+caps['browserstack.local'] = 'true'
+caps['browserstack.localIdentifier'] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
+caps['os'] = ENV['BS_AUTOMATE_OS']
+caps['os_version'] = ENV['BS_AUTOMATE_OS_VERSION']
+caps['browser'] = ENV['SELENIUM_BROWSER']
+caps['browser_version'] = ENV['SELENIUM_VERSION']
+caps['browserstack.debug'] = "true"
+
+caps['project'] = ENV['BS_AUTOMATE_PROJECT'] if ENV['BS_AUTOMATE_PROJECT']
+caps['build'] = ENV['BS_AUTOMATE_BUILD'] if ENV['BS_AUTOMATE_BUILD']
+
+browser = Selenium::WebDriver.for(:remote,
+  :url => "http://joostbaaij1:#{ENV['BROWSERSTACK_ACCESS_KEY']}@hub-cloud.browserstack.com/wd/hub",
+  :desired_capabilities => caps)
+
+Before do |scenario|
+  @browser = browser
+end
+
+at_exit do
+  browser.quit
+end
