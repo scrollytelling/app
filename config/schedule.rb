7a57acfe22893345d995e5c919dd7b87d6c4a591
Joost Baaij <joost@spacebabies.nl>
2017-11-14 22:03:37 +0100

    yikes, of course whenever needs a sgtring param

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ env :PATH, '/usr/local/bin:/usr/bin:$PATH'
 set :output, 'log/cron.log'
 
 every :month do
-  rake pageflow:prune_auto_snapshots_jobs:enqueue
+  rake "pageflow:prune_auto_snapshots_jobs:enqueue"
 end
 
 # * * * * *  command to execute
