b9f4c795bb387e347fe778f4149afed5348bf7eb
Joost Baaij <joost@spacebabies.nl>
2017-11-28 17:06:21 +0100

    add OkComputer check for shared uploads directory

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,11 +1,14 @@
 require 'ok_computer/built_in_checks/resque_down_check'
 require 'ok_computer/built_in_checks/resque_backed_up_check'
+require 'ok_computer/built_in_checks/directory_check'
 
-OkComputer::Registry.register "resque_down", OkComputer::ResqueDownCheck.new
-
+OkComputer::Registry.register "resque_down",
+  OkComputer::ResqueDownCheck.new
 OkComputer::Registry.register "resque_backed_up",
   OkComputer::ResqueBackedUpCheck.new("resizing", 100)
 OkComputer::Registry.register "resque_backed_up",
   OkComputer::ResqueBackedUpCheck.new("default", 100)
 OkComputer::Registry.register "resque_backed_up",
   OkComputer::ResqueBackedUpCheck.new("slow", 500)
+OkComputer::Registry.register "shared_uploads",
+  OkComputer::DirectoryCheck.new("/efs/uploads")
