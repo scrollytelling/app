71805780fa0cb0a355ffd958368a06b33d178f95
Joost Baaij <joost@spacebabies.nl>
2018-08-22 15:07:58 +0200

    remove our Piwik tracker

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -2,17 +2,14 @@ require 'yaml'
 
 # Render client-side analytics scripts for published entries.
 module AnalyticsHelper
-  def scrolly_analytics_script_tag(entry)
-    render partial: "analytics/piwik",
-      locals: {entry: entry, account: entry.account}
-  end
-
   def google_analytics_script_tag(entry)
     file = Rails.root.join('config/analytics/google_analytics.yml')
     analytics = YAML.load_file(file)
     theme = entry.theming.theme_name
 
-    render partial: "analytics/google_analytics",
-      locals: {analytics: analytics[theme]} if analytics[theme]
+    if analytics[theme]
+      render partial: 'analytics/google_analytics',
+             locals: { analytics: analytics[theme] }
+    end
   end
 end
